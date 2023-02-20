e7033868f0fb9b12443bfb6ffcdc4f1cc2c515d3
Nahuel Benitez <jorgenahuelbenitez07@gmail.com>
2023-02-20 18:11:41 -0300

    agregue el contenedor de las cards, y en app.js la funcion para visualizar las pelis

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// clave = "5b197352180c9d801b6d9d654b40a762";
+
+const API_URL="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5b197352180c9d801b6d9d654b40a762&page=1";
+const IMAGE_PATH="https://image.tmdb.org/t/p/w500/";
+const SEARCH_URL="https://api.themoviedb.org/3/search/movie?api_key=5b197352180c9d801b6d9d654b40a762&include_adult=false&query='";
+
+//https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5b197352180c9d801b6d9d654b40a762
+
+
+
+let card = document.getElementById('card-template');
+
+
+
+async function getMovies(url){
+    const res= await fetch(url);
+    const data = await res.json()
+    displayMovies(data.results);
+    console.log(data.results);
+
+    
+    }
+
+function displayMovies(movies){
+    movies.forEach((movie)=>{
+        const {title, poster_path, overview}=movie;
+        
+        card.innerHTML+=
+        `
+        <div class="col">
+          <div class="card">
+          <img src="${IMAGE_PATH +poster_path}" alt="${title}" />
+            <div class="card-body">
+              <h5 class="card-title">${title}</h5>
+              <p class="card-text">${overview}</p>
+            </div>
+          </div>
+        </div>
+        `;
+    })
+}
+
+getMovies(API_URL)    
